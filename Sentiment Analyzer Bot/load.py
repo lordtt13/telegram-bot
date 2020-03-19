4b2662fab48413f1ff6449d2a5e16a211bb32c3a
lordtt13 <thakurtanmay72@yahoo.com>
2020-03-19 08:05:12 +0530

    Conv algorithm failed on cuda fixed
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- Sentiment Analyzer Bot/load.py
+++ Sentiment Analyzer Bot/load.py
@@ -5,7 +5,15 @@ Created on Thu Sep 12 23:37:33 2019
 @author: tanma
 """ 
 import tensorflow as tf
-from tensorflow.keras.models import load_model   
+
+from tensorflow.keras.models import load_model 
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
 
 def init():
     model = load_model('toxic_model.h5')
